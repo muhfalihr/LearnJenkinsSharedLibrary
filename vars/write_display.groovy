1d0da84c5c30c168451618c054eb23ad0d079ca3
muhfalihr <muhammadfalihromadhoni@gmail.com>
2024-06-05 15:27:41 +0700

    add write_display function [fixing]

--- vars/write_display.groovy
+++ vars/write_display.groovy
@@ -1,8 +1,8 @@
-def call(steps, List file_names) {
-    for (fn in file_names) {
+def call(steps, List<String> fileNames) {
+    fileNames.each { fn ->
         def filename = "${fn}.txt"
         def content = "This is ${filename} file."
-        steps.sh(script: "echo ${content} > ${filename}")
+        steps.sh(script: "echo '${content}' > ${filename}")
         steps.sh(script: "cat ${filename}")
     }
 }
